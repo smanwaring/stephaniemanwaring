c7f086b2b0a2dc80a78974a2aaee47cc4f65132c
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2017-02-23 00:35:27 -0500

    fixed spacing on projects

--- browser/components/projects/projects.component.js
+++ browser/components/projects/projects.component.js
@@ -3,34 +3,34 @@ import projects from './projects-list';
 
 export default () => (
 	<div className="spacer">
-				{ projects.map( project => {
-					return (
-						<div key={project.id} className="wrapper bottom-spacer">
-							<aside className="aside aside-1">
-								<ul className="flex-container">
-									<li className={`video-lg-${project.id}`}></li>
-								</ul>
-							</aside>
-						<article className="main">
-							<div className="project-head">{project.title}</div>
-							<div className="main-text-project">
-								{project.description}
-							</div>
-							<br/>
-							<div className="main-text-project">
-								Powered by: {project.poweredBy}
-								</div>
-							<br/>
-							{ project.links.map( link => {
-								return (
-									<a key={link.url} href={link.url}><div className="link-box">{link.display}</div></a>
-								);
-							})
-							}
-						</article>
+		{ projects.map( project => {
+			return (
+				<div key={project.id} className="wrapper bottom-spacer">
+					<aside className="aside aside-1">
+						<ul className="flex-container">
+							<li className={`video-lg-${project.id}`}></li>
+						</ul>
+					</aside>
+				<article className="main">
+					<div className="project-head">{project.title}</div>
+					<div className="main-text-project">
+						{project.description}
 					</div>
-					);
-				})}
+					<br/>
+					<div className="main-text-project">
+						Powered by: {project.poweredBy}
+						</div>
+					<br/>
+					{ project.links.map( link => {
+						return (
+							<a key={link.url} href={link.url}><div className="link-box">{link.display}</div></a>
+						);
+					})
+					}
+				</article>
+			</div>
+			);
+		})}
 	</div>
 );
 
